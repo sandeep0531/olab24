444d1ba9f44a89734651a21abec5af82d4f461d2
sandeep0531 <166251464+sandeep0531@users.noreply.github.com>
2024-10-27 15:59:30 +0530

    Update IBook.java

--- DOP/src/newshelf/IBook.java
+++ DOP/src/newshelf/IBook.java
@@ -3,3 +3,32 @@ package newshelf;
 public interface IBook {
 
 }
+enum FictionType {
+
+	Comedy,
+	Tragedy,
+	GoK
+}
+   record TextBook(String subject) implements IBook{
+      public String toString() {
+         return "Subject of the TextBook : " + this.subject + "\n";
+      }
+   }
+    record Comic(String title,int ageOfMainCharacter) implements IBook{
+      @Override
+      public String toString() {
+         return "Title: "+this.title + "\nAge of Main Character: " + this.ageOfMainCharacter;
+      }
+   
+       
+      @Override
+      public int hashCode() {
+         return this.title.hashCode()+ageOfMainCharacter;
+      }
+   }
+    record Fiction(String name,FictionType type) implements IBook{
+      @Override
+      public String toString() {
+         return "Name : " + this.name + "\nFictional Type : " + type + "\n";
+      }
+   }
