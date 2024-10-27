f1a242398c48d08c78298ea442a3bf63ecaab52a
sandeep0531 <166251464+sandeep0531@users.noreply.github.com>
2024-10-27 16:06:19 +0530

    Update OldSelection.java

--- DOP/src/oldshelf/OldSelection.java
+++ DOP/src/oldshelf/OldSelection.java
@@ -1,26 +1,42 @@
 package oldshelf;
 
 public class OldSelection {
-
-	// TODO: Complete this method.
-	/**
-	 * 
-	 * @param o object
-	 * returns if o is not a book, returns empty string, if Comic, returns title, of Fiction
-	 * returns name, and if TextBook, returns subject.
-	 */
 	public static String getAgeOrTitle(Object o) {
 		
 		if (o instanceof Book) {
-			
+			if(o instanceof Comic)
+			{
+				return ((Comic)o).getTitle();
+			}
+			else if(o instanceof Fiction)
+			{
+				return ((Fiction)o).getName();
+			}
+			else if(o instanceof TextBook)
+			{
+				return ((TextBook)o).getSubject();
+			}
+			else
+			  return null;
+
 		}
 		return null;
 	}
 
 	public static void main(String[] args) {
+		TextBook t = new TextBook("Object Oriented Systems");
+		Comic c = new Comic("Batman: The Killing Joke", 30);
+		Fiction f = new Fiction("Good Omens", FictionType.Comedy);
+		System.out.println("Verifying ToString method of books : ");
+		System.out.println(t.toString() + "\n");
+		System.out.println(c.toString() + "\n");
+		System.out.println(f.toString() + "\n");
 		
-		// TODO: Write a test code here and execute and text.
-		TextBook t = new TextBook();
-		
+
+		System.out.println("Using  getAgeorTItle method : ");
+		System.out.println("getting  getAgeorTItle for textbook : " + getAgeOrTitle(t) + "\n");
+		System.out.println("getting  getAgeorTItle for comic: " + getAgeOrTitle(c) + "\n");
+		System.out.println("getting  getAgeorTItle for Fiction : " + getAgeOrTitle(f) + "\n");
+
 	}
 }
