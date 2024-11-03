c9531f975aa5d1dfe03a60e8f4b9963ca675faf9
sandeep0531 <166251464+sandeep0531@users.noreply.github.com>
2024-11-03 22:50:39 +0530

    Update GenericComic.java

--- DOP/src/junitstest/newgenerictypes/GenericComic.java
+++ DOP/src/junitstest/newgenerictypes/GenericComic.java
@@ -1,1 +1,23 @@
 
+package newgenerictypes;
+
+/*public record GenericComic<T>(T title, int ageOfMainCharacter) implements IGenericBook<T> {
+    @Override
+    public T getTitleOrSubject() {
+        return title;
+    }
+}*/
+public record GenericComic<T extends Comparable<T>>(T title, int ageOfMainCharacter) implements IGenericBook<T> {
+    @Override
+    public T getTitleOrSubject() {
+        return title;
+    }
+
+    @Override
+    public int compareTo(IGenericBook<T> other) {
+        if (other instanceof GenericComic<T> otherComic) {
+            return this.title.compareTo(otherComic.title);
+        }
+        return 0; // Default to equal if other types are compared
+    }
+}
